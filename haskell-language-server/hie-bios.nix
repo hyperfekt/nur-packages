76bafbb5971addf796e3919b961bd3e929e1e716
hyperfekt <git@hyperfekt.net>
2020-06-11 18:59:52 +0200

    update hie-bios to version with shell cradle

--- haskell-language-server/hie-bios.nix
+++ haskell-language-server/hie-bios.nix
@@ -1,14 +1,17 @@
 let
-  f = { callHackageDirect
+  f = { callCabal2nix
+      , fetchFromGitHub
       , dontCheck
       , ...}@args:
       dontCheck
-        (callHackageDirect
-          {
-            pkg = "hie-bios";
-            ver = "0.5.0";
-            sha256 = "116nmpva5jmlgc2dgy8cm5wv6cinhzmga1l0432p305074w720r2";
-          }
+        (callCabal2nix
+          "hie-bios"
+          (fetchFromGitHub {
+            owner = "mpickering";
+            repo = "hie-bios";
+            rev = "293baaf793f82f639affe734ebe785ccdcafd40b";
+            sha256 = "04hp32xzbqij4q6npybx2xbzy8x84a2i2r7zxfhks2fx5xjg11vf";
+          })
           (builtins.removeAttrs args (builtins.attrNames (builtins.functionArgs f))));
 in
   f
